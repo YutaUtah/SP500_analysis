423660ec33802de53f12866d36319887aebbeb7c
Yuta Hayashi <yuta.hayashi.soccer@gmail.com>
2020-05-02 23:35:53 +0900

    Merge branch 'master' of https://github.com/YutaUtah/SP500_analysis
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- sp500.py
+++ sp500.py
@@ -9,6 +9,9 @@ import numpy as np
 import pandas_datareader.data as web
 import pickle
 import requests
+from collections import Counter
+
+
 
 
 
@@ -144,7 +147,41 @@ def buy_sell_hold(*args):
             return -1
     return 0
 
+
+
 def extract_featuresets(ticker):
     tickers, df = process_data_for_labels(ticker)
 
-    # df["{}_target".format(ticker)] = l
+    df["{}_target".format(ticker)] = list(map(buy_sell_hold,
+                                              df["{}_1d".format(ticker)],
+                                              df["{}_2d".format(ticker)],
+                                              df["{}_3d".format(ticker)],
+                                              df["{}_4d".format(ticker)],
+                                              df["{}_5d".format(ticker)],
+                                              df["{}_6d".format(ticker)],
+                                              df["{}_7d".format(ticker)]))
+
+    vals = df["{}_target".format(ticker)].values.tolist()
+    str_vals = [str(i) for i in vals]
+    print("Data spread: ", Counter(str_vals))
+    df.fillna(0, inplace = True)
+
+    # replace inf / -inf with nan
+    df = df.replace([np.inf, -np.inf], np.nan)
+    # drop na for above function
+    df.dropna(inplace = True)
+
+    df_vals = df[[ticker for ticker in tickers]].pct_change()
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
+    df_vals.fillna(0, inplace = True)
+
+    X = df_vals.values
+    y = df["{}_target".format(ticker)].values
+
+    return X, y
+
+
+extract_featuresets("MMM")
+
+
+
